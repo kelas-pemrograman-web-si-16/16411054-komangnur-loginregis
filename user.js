e2202641ef7540d0d7a4cfa1597b8da3ab8b090e
komangnurkertiani <komang.16411054@student.ubl.ac.id>
2019-06-18 13:19:06 +0700

    first commit

--- user.js
+++ user.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/registrasi', function(req, res, next) {
-    res.render('registrasi', {nama: 'KomangNurKertiani'})
+    res.render('registrasi', {nama: 'ReniAprillia'})
 })
 
 module.exports = router;
